114e1d9d2edeca39d83b7f0eb3728bade129c563
Chimch <timothee.consigny@dej-apps.com>
2015-11-03 00:09:46 +0100

    fuckkk

--- nbproject/Package-default.bash
+++ nbproject/Package-default.bash
@@ -10,9 +10,9 @@ CND_CONF=default
 CND_DISTDIR=dist
 TMPDIR=build/${CND_CONF}/${IMAGE_TYPE}/tmp-packaging
 TMPDIRNAME=tmp-packaging
-OUTPUT_PATH=dist/${CND_CONF}/${IMAGE_TYPE}/Proto2_Magneto.X.${IMAGE_TYPE}.${OUTPUT_SUFFIX}
-OUTPUT_BASENAME=Proto2_Magneto.X.${IMAGE_TYPE}.${OUTPUT_SUFFIX}
-PACKAGE_TOP_DIR=proto2magneto.x/
+OUTPUT_PATH=dist/${CND_CONF}/${IMAGE_TYPE}/KompassDevice.${IMAGE_TYPE}.${OUTPUT_SUFFIX}
+OUTPUT_BASENAME=KompassDevice.${IMAGE_TYPE}.${OUTPUT_SUFFIX}
+PACKAGE_TOP_DIR=kompassdevice/
 
 # Functions
 function checkReturnCode
@@ -57,15 +57,15 @@ mkdir -p ${TMPDIR}
 
 # Copy files and create directories and links
 cd "${TOP}"
-makeDirectory ${TMPDIR}/proto2magneto.x/bin
+makeDirectory ${TMPDIR}/kompassdevice/bin
 copyFileToTmpDir "${OUTPUT_PATH}" "${TMPDIR}/${PACKAGE_TOP_DIR}bin/${OUTPUT_BASENAME}" 0755
 
 
 # Generate tar file
 cd "${TOP}"
-rm -f ${CND_DISTDIR}/${CND_CONF}/package/proto2magneto.x.tar
+rm -f ${CND_DISTDIR}/${CND_CONF}/package/kompassdevice.tar
 cd ${TMPDIR}
-tar -vcf ../../../../${CND_DISTDIR}/${CND_CONF}/package/proto2magneto.x.tar *
+tar -vcf ../../../../${CND_DISTDIR}/${CND_CONF}/package/kompassdevice.tar *
 checkReturnCode
 
 # Cleanup
